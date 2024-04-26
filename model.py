2b120477e1b5bf95c3982f2259513f43fe8ec90d
Mayank <23f2005461@ds.study.iitm.ac.in>
2024-04-26 12:14:39 +0530

    models.py/database

---
+++ model.py
@@ -0,0 +1,25 @@
+from flask_login import UserMixin
+from flask_wtf import FlaskForm
+
+
+class User(UserMixin):
+    def __init__(self, user_data):
+        self.id = str(user_data['_id'])
+        self.username = user_data['username']
+        self.email = user_data['email']
+        self.password = user_data['password']
+
+    def get_id(self):
+        return self.id
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+
+
